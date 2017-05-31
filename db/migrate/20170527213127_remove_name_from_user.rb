fa1d2b9646c4a59705f36afa5bc7642a61bf2e79
RobertEikeland <eikrob15@student.westerdals.no>
2017-05-31 02:50:18 +0200

    Finished forum + more validation

--- db/migrate/20170527213127_remove_name_from_user.rb
+++ db/migrate/20170527213127_remove_name_from_user.rb
@@ -1,11 +1,11 @@
 class RemoveNameFromUser < ActiveRecord::Migration[5.0]
-  def self.up
-    remove_column :users, :name, :string
-    add_column :users, :username, :string
-  end
-
-  def self.down
-    add_column :users, :name, :string
-    remove_column :users, :username, :string
-  end
+  # def self.up
+  #   remove_column :users, :name, :string
+  #   add_column :users, :username, :string
+  # end
+  #
+  # def self.down
+  #   add_column :users, :name, :string
+  #   remove_column :users, :username, :string
+  # end
 end
